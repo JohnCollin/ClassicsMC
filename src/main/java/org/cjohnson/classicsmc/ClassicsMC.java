070161e83b36665ea279fb2c32186a6fe12d0e0f
JohnCollin <collin.g.johnson@gmail.com>
2020-01-24 12:59:44 -0500

    Going to use EssentialsSpawn, easier.

--- src/main/java/org/cjohnson/classicsmc/ClassicsMC.java
+++ src/main/java/org/cjohnson/classicsmc/ClassicsMC.java
@@ -1,7 +1,6 @@
 package org.cjohnson.classicsmc;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.cjohnson.classicsmc.command.CommandSpawn;
 import org.cjohnson.classicsmc.listener.PlayerTrafficListener;
 
 public class ClassicsMC extends JavaPlugin {
@@ -9,8 +8,6 @@ public class ClassicsMC extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		this.saveDefaultConfig();
-		
-		getCommand("spawn").setExecutor(new CommandSpawn(this));
 		
 		getServer().getPluginManager().registerEvents(new PlayerTrafficListener(this), this);
 	}
